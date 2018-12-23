09020d92feb8c83c7c99f98494118f7623cade9e
Magnus Dahlgren <hello@magnusdahlgren.com>
2018-12-23 08:23:08 +0000

    Adding slot machine

---
+++ dev/slotmachine/Reels.js
@@ -0,0 +1,47 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Reels = function (_React$Component) {
+    _inherits(Reels, _React$Component);
+
+    function Reels(props) {
+        _classCallCheck(this, Reels);
+
+        return _possibleConstructorReturn(this, (Reels.__proto__ || Object.getPrototypeOf(Reels)).call(this, props));
+    }
+
+    _createClass(Reels, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { id: 'reels' },
+                this.renderWheel(this.props.symbols[this.props.reels[0]]),
+                this.renderWheel(this.props.symbols[this.props.reels[1]]),
+                this.renderWheel(this.props.symbols[this.props.reels[2]])
+            );
+        }
+    }, {
+        key: 'renderWheel',
+        value: function renderWheel(symbol) {
+            return React.createElement(
+                'div',
+                { className: 'reel' },
+                React.createElement(
+                    'span',
+                    { className: 'symbol symbol-' + symbol },
+                    symbol
+                )
+            );
+        }
+    }]);
+
+    return Reels;
+}(React.Component);
+
+export default Reels;
